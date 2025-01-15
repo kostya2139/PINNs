5ae9e367e726c3497b63d14d2e91ef3653d4a883
kostya2139 <97086581+kostya2139@users.noreply.github.com>
2025-01-15 20:21:10 +0100

    Update Heat_equation.py

--- Heat_equation.py
+++ Heat_equation.py
@@ -1,7 +1,24 @@
 import tensorflow as tf
 import numpy as np
 
+def initial():
+    np.random.seed(0)
+    tf.random.set_seed(0)
+
+
 class PINNs:
-    def __init__():
-        pass
+    def __init__(self, n_neurons, n_layers, alpha, ini, boun):
+        self.n_neurons = n_neurons
+        self.n_layers = n_layers
+        self.alpha = alpha
+        self.x_ini = ini[0:1, :]
+
+
+
+
+def main(ini, boun):
+    n_neurons = 10
+    n_layers = 10
+    initial_conditions, boundary_conditions = ini, boun # [x, t, u]
+
 
