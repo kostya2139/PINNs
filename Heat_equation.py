73c153a63b3a60ea0c68480e1fe6978a29d7af59
kostya2139 <97086581+kostya2139@users.noreply.github.com>
2025-01-15 21:13:20 +0100

    init

--- Heat_equation.py
+++ Heat_equation.py
@@ -7,11 +7,67 @@ def initial():
 
 
 class PINNs:
-    def __init__(self, n_neurons, n_layers, alpha, ini, boun):
+    def __init__(self, n_neurons, n_layers, alpha, u, f):
         self.n_neurons = n_neurons
         self.n_layers = n_layers
         self.alpha = alpha
-        self.x_ini = ini[0:1, :]
+        self.x_u = u[:, 0:1]
+        self.t_u = u[:, 1:2]
+        self.u = u[:, 2:3]
+        self.x_f = f[:, 0:1]
+        self.t_f = f[:, 1:2]
+        self.layers = [2] + n_layers * [n_neurons] + [1]
+
+        self.weights, self.biases = self.initialize_NN(self.layers)
+
+        self.sess = tf.Session(config=tf.ConfigProto(allow_soft_placement=True,
+                                                     log_device_placement=True))
+        
+        self.x_u_tf = tf.placeholder(tf.float32, shape=[None, 1])
+        self.t_u_tf = tf.placeholder(tf.float32, shape=[None, 1])
+        self.u_tf = tf.placeholder(tf.float32, shape=[None, 1])
+        self.x_f_tf = tf.placeholder(tf.float32, shape=[None, 1])
+        self.t_f_tf = tf.placeholder(tf.float32, shape=[None, 1])        
+                
+        self.u_pred = self.net_u(self.x_u_tf, self.t_u_tf) 
+        self.f_pred = self.net_f(self.x_f_tf, self.t_f_tf) 
+
+        self.loss = tf.reduce_mean(tf.square(self.u_tf - self.u_pred)) + \
+                    tf.reduce_mean(tf.square(self.f_pred))
+               
+                
+        self.optimizer = tf.contrib.opt.ScipyOptimizerInterface(self.loss, 
+                                                                method = 'L-BFGS-B', 
+                                                                options = {'maxiter': 50000,
+                                                                           'maxfun': 50000,
+                                                                           'maxcor': 50,
+                                                                           'maxls': 50,
+                                                                           'ftol' : 1.0 * np.finfo(float).eps})
+        
+        init = tf.global_variables_initializer()
+        self.sess.run(init)
+
+        
+
+
+
+
+    def initialize_NN(self, layers):        
+        weights = []
+        biases = []
+        num_layers = len(layers) 
+        for l in range(0,num_layers-1):
+            W = self.xavier_init(size=[layers[l], layers[l+1]])
+            b = tf.Variable(tf.zeros([1,layers[l+1]], dtype=tf.float32), dtype=tf.float32)
+            weights.append(W)
+            biases.append(b)        
+        return weights, biases
+    
+    def xavier_init(self, size):
+        in_dim = size[0]
+        out_dim = size[1]        
+        xavier_stddev = np.sqrt(2/(in_dim + out_dim))
+        return tf.Variable(tf.truncated_normal([in_dim, out_dim], stddev=xavier_stddev), dtype=tf.float32)
 
 
 
